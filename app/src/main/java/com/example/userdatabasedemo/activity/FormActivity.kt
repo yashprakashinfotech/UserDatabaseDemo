d82013cbe4ab738556a6e296f63cc892c9779005
yash.patel <yash@prakashinfotech.com>
2022-04-14 18:01:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/userdatabasedemo/activity/FormActivity.kt
@@ -0,0 +1,12 @@
+package com.example.userdatabasedemo.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.userdatabasedemo.R
+
+class FormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
---
+++ app/src/main/java/com/example/userdatabasedemo/adapter/UserAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.userdatabasedemo.adapter
+
+import android.content.Context
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class UserAdapter(val context: Context) {
+
+    class UserViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+    }
+}
